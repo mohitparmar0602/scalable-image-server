a7612458a81c2f94793112cfd7dd514e7958aef5
Mohit <mohitparmar22584@gmail.com>
2026-05-02 19:36:25 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -94,5 +94,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port number: ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
